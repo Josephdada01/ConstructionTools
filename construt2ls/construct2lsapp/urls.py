3970551d6f1f5bc19a08b9530072500838e8c5c6
Josephdada01 <ridwandada500@gmail.com>
2024-02-27 13:31:06 +0100

    created the about and contact function, and render it in views

--- construt2ls/construct2lsapp/urls.py
+++ construt2ls/construct2lsapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from construct2lsapp import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('contact', views.contact, name="contact"),
+    path('about', views.about, name="about"),
 
 ]
--- construt2ls/construct2lsapp/views.py
+++ construt2ls/construct2lsapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def contact(request):
+    return render(request, "contact.tml")
+
+def about(request):
+    return render(request, "about.html")
