11b0d008277cf505764ac46667e56c99bf782c30
Josephdada01 <ridwandada500@gmail.com>
2024-02-27 18:10:22 +0100

    integrating django template

--- construt2ls/construct2lsapp/views.py
+++ construt2ls/construct2lsapp/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, "index.html")
 
 def contact(request):
-    return render(request, "contact.tml")
+    return render(request, "contact.html")
 
 def about(request):
     return render(request, "about.html")
