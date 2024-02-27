404bc7f6082baecd94b32f8a471c5b928cb2c694
Josephdada01 <ridwandada500@gmail.com>
2024-02-27 19:35:17 +0100

    adding the tempo template

--- construt2ls/static/assets/vendor/glightbox/js/glightbox.js
+++ construt2ls/static/assets/vendor/glightbox/js/glightbox.js
@@ -380,9 +380,9 @@
       }, timeout);
     }
   }
-  function injectAssets(url, waitFor, callback) {
+  function injectstatic/assets(url, waitFor, callback) {
     if (isNil(url)) {
-      console.error('Inject assets error');
+      console.error('Inject static/assets error');
       return;
     }
 
@@ -1785,12 +1785,12 @@
     addClass(slideContainer, 'gvideo-container');
     slideMedia.insertBefore(createHTML('<div class="gvideo-wrapper"></div>'), slideMedia.firstChild);
     var videoWrapper = slide.querySelector('.gvideo-wrapper');
-    injectAssets(this.settings.plyr.css, 'Plyr');
+    injectstatic/assets(this.settings.plyr.css, 'Plyr');
     var url = data.href;
     var provider = data === null || data === void 0 ? void 0 : data.videoProvider;
     var customPlaceholder = false;
     slideMedia.style.maxWidth = data.width;
-    injectAssets(this.settings.plyr.js, 'Plyr', function () {
+    injectstatic/assets(this.settings.plyr.js, 'Plyr', function () {
       if (!provider && url.match(/vimeo\.com\/([0-9]*)/)) {
         provider = 'vimeo';
       }
