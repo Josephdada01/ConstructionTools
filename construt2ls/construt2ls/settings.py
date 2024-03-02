32226724971ff48eb6f982dc4b992c21bd28b931
Josephdada01 <ridwandada500@gmail.com>
2024-03-02 15:40:46 +0100

    making changes

--- construt2ls/construt2ls/settings.py
+++ construt2ls/construt2ls/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,9 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# for alerting error color
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
--- construt2ls/custom_auth/views.py
+++ construt2ls/custom_auth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def signup(request):
@@ -8,7 +9,8 @@ def signup(request):
         password = request.POST['pass_word']
         confirm_pass = request.POST['confirm_password']
         if password != confirm_pass:
-            return HttpResponse("make sure password matches confirm password")
+            messages.warning(request, "Password not Matching")
+            return render(request, 'signup.html')
         
         try:
             if User.objects.get(username=email):
