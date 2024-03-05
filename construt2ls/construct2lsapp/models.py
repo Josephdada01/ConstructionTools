a76aa420985f5c5200fff43afb5fb3f977930801
Josephdada01 <ridwandada500@gmail.com>
2024-03-05 23:34:54 +0100

    creating product class for my products

--- construt2ls/construct2lsapp/models.py
+++ construt2ls/construct2lsapp/models.py
@@ -9,4 +9,18 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    desc = models.CharField(max_length=300)
+
+    image = models.ImageField(upload_to='images/images', default="")
+
+    def __str__(self):
+        return self.product_name
 
