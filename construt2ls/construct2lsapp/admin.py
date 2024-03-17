36a2222dc8cd85e5e47314fe3a0dd06656432aa2
Josephdada01 <ridwandada500@gmail.com>
2024-03-17 22:27:29 +0100

    Initial commit for Heroku deployment

--- construt2ls/construct2lsapp/admin.py
+++ construt2ls/construct2lsapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from construct2lsapp.models import Contact, Product, Orders, OrderUpdate
-# Register your models here.
+# Register your models here here.
 admin.site.register(Contact)
 admin.site.register(Product)
 admin.site.register(Orders)
